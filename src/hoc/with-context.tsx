24acfe361a7be610e8e00e4d7019e2dc66d9cc43
Christophe Duchesne-Ashworth <christophe.ashworth@unicornpowered.com>
2021-11-04 11:06:17 -0400

    add with-context hoc

---
+++ src/hoc/with-context.tsx
@@ -0,0 +1,18 @@
+import React, { Context, FunctionComponent } from 'react'
+
+type ContextProvider<C> = () => C
+
+export function withContext<C, P>(
+  contextProvider: ContextProvider<C>,
+  context: Context<C>,
+): (WrappedComponent: FunctionComponent<P>) => FunctionComponent<P> {
+  return (WrappedComponent: FunctionComponent<P>): FunctionComponent<P> => {
+    const ComponentWithContext: FunctionComponent<P> = (props: P) => (
+      <context.Provider value={contextProvider()}>
+        <WrappedComponent {...props} />
+      </context.Provider>
+    )
+
+    return ComponentWithContext
+  }
+}
